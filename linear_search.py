2936e531f2e2e59cf9416b4fd0e0d4d74607e767
khmahmud101 <kmahmud101@gmail.com>
2022-01-02 11:41:01 +0600

    revise linear search

--- linear_search.py
+++ linear_search.py
@@ -4,19 +4,21 @@ def linear_search(L,x):
     while i < n:
         if L[i] == x:
             return i
-        i +=1
-    i = -1
-    return i
+        i += 1
+    return -1
 
-print(linear_search([1,2,3,4,5],10))
+result = linear_search([1,2,3,4,5],3)
+if result == -1:
+    print("Element Not found")
+print("Element found at index", result)
 
-def linear_search(L,x):
-    n = len(L)
-    for i in range(n):
-        if L[i] == x:
-            return i
-
-    i = -1
-    return i
-
-print(linear_search([1,2,3,4,5],3))
+# def linear_search(L,x):
+#     n = len(L)
+#     for i in range(n):
+#         if L[i] == x:
+#             return i
+#
+#     i = -1
+#     return i
+#
+# print(linear_search([1,2,3,4,5],3))
