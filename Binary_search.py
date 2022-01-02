992eaef3be87313d539e620190615a83040affbf
khmahmud101 <kmahmud101@gmail.com>
2022-01-02 11:42:02 +0600

    revise binary search

--- Binary_search.py
+++ Binary_search.py
@@ -1,41 +1,32 @@
-#n = int(input())
-nums = ['1', '2', '3']
-#for i in range(n):
-    #nums.append (input("enter num: "))
-#print(nums)
-#print(nums[2])
-list_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-element = 3
-print(list_numbers.index(4))
-def binary_search(L,x):
-    left = 0
-    right = len(L)-1
+def binary_search(L, x):
+    left, right = 0, len(L) - 1
     while left <= right:
-        mid = (left+right)//2
+        mid = (left + right)//2
         if L[mid] == x:
-            return L[mid]
-        if L[mid] < x:
-            left = mid +1
+            return mid
+
+        if x < L[mid]:
+            right = mid - 1
         else:
-            right = mid -1
+            left = mid + 1
     return -1
-#print(binary_search([1,2,3,4,5,6,7],9))
 
 if __name__ == "__main__":
-    L = [1,2,3,4,5,6,7,8,9,]
-    for x in range(1,100):
-        position = binary_search(L,x)
+    L = [1,2,3,4,5,6,7,8,9]
+    for x in range(1,11):
+        position = binary_search(L, x)
         if position == -1:
             if x in L:
-                print(x, "is in L, but function return -1")
+                print(x,"is in L, but function returned -1")
             else:
-                print(x,"not in list")
+                print("Element Not in list")
         else:
-            if position == x:
-                print(x,"found in correct position")
+            if L[position] == x:
+                print(x, "found at correct position." )
             else:
-                print("Binary search returned", position, "for",x, "which is incorrect")
-    print("program Terminated")
+                print("binary search returned ", position, "for", x, "which is incorrect")
+
+
 
 
 
